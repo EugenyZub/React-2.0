8584b2f2649ebd5975c6074fd4d1fc4bd6cf33ba
Ëugene <evgeniys1@tut.by>
2019-06-12 08:22:54 +0300

    some changes

--- Lesson3/my-app/src/components/app/app.js
+++ Lesson3/my-app/src/components/app/app.js
@@ -9,6 +9,14 @@ import PostAddForm from '../post-add-form';
 import './app.css';
 
 const App = () => {
+
+    const data = [
+        {label: 'Going to learn React', important: true, id: 'wewq'},
+        {label: 'That is so good', important: false, id: 'dsd'},
+        {label: 'I neeed a break...', important: false, id: 'wqfu'}
+    ];
+
+
     return(
         <div className='app'>
             <AppHeader/>
@@ -16,9 +24,8 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data}/>
             <PostAddForm/>
-            <AppHeader/>
         </div>
         
     )
