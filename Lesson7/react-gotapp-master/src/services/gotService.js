ea9644513e382bbcead488de05ed85db3b625c59
Eugene <evgeniys1@tut.by>
2019-06-17 15:50:10 +0300

    recieved all

--- Lesson7/react-gotapp-master/src/services/gotService.js
+++ Lesson7/react-gotapp-master/src/services/gotService.js
@@ -12,21 +12,55 @@ export default class GotService {
 
         return await res.json();
     };
+
     getAllCharacters() {
         return this.getResource('/characters?page=5&pageSize=10');
     }
-
     getCharacter(id) {
         return this.getResource(`/characters/${id}`);
+    }
+
+    //books
+    getAllBooks() {
+        return this.getResource('/books?page=1');
+    }
+    getBook(id) {
+        return this.getResource(`/books/${id}`);
+    }
+
+    //books
+    getAllHouses() {
+        return this.getResource('/houses?page=4');
+    }
+    getHouse(id) {
+        return this.getResource(`/houses/${id}`);
     }
 }
 
 const got = new GotService();
-
+//Персонажи
 got.getAllCharacters()
     .then(res => {
         res.forEach( item => console.log(item.name));
     });
 
 got.getCharacter(130)
+    .then(res => console.log(res));
+
+//Книги
+got.getAllBooks()
+    .then(res => {
+        res.forEach( item => console.log(item.name));
+    });
+
+got.getBook(4)
+    .then(res => console.log(res));
+
+//дома
+got.getAllHouses()
+    .then(res => {
+        res.forEach( item => console.log(item.name));
+    });
+
+got.getHouse(8)
     .then(res => console.log(res));
